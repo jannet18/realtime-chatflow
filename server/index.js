57e449c20dde3d9e79c0181ac6c04e9b37e2cf84
jannet18 <janetngei18@gmail.com>
2025-06-19 00:49:33 +0300

    path configurations

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
