d3d37f2b25ce85de90a72cb6ee9fff4a3e54b075
jannet18 <janetngei18@gmail.com>
2025-06-19 01:17:10 +0300

    serve static file

--- server/index.js
+++ server/index.js
@@ -20,8 +20,13 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 
+// Serve static files from frontend build
+const frontendPath = path.join(__dirname, "./frontend/dist");
+app.use(express.static(frontendPath));
+
+// Catch-all route to serve index.html for SPA
 app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, frontendPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
